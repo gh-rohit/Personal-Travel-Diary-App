34db4696c05beaed400852d28c3e70df1d755f73
Cenote <himangshukalita131@gmail.com>
2025-03-28 09:51:24 +0530

    Image uploading api route

--- backend/routes/travelStory.route.js
+++ backend/routes/travelStory.route.js
@@ -3,9 +3,13 @@ import { verifyToken } from "../utils/verifyUser.js"
 import {
   addTravelStory,
   getAllTravelStory,
+  imageUpload,
 } from "../controllers/travelStory.controller.js"
+import upload from "../multer.js"
 
 const router = express.Router()
+
+router.post("/image-upload", upload.single("image"), imageUpload)
 
 router.post("/add", verifyToken, addTravelStory)
 
