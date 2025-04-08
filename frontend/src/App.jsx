31186d6e84dde65f3542d6cf6b4f1f00d9e348b4
Cenote <himangshukalita131@gmail.com>
2025-04-08 09:49:10 +0530

    Sign up UI and integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home/Home"
 import Login from "./pages/Auth/Login"
 import SignUp from "./pages/Auth/SignUp"
+import PrivateRoute from "./components/PrivateRoute"
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" exact element={<Home />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/" exact element={<Home />} />
+          </Route>
+
           <Route path="/login" exact element={<Login />} />
           <Route path="/sign-up" exact element={<SignUp />} />
         </Routes>
